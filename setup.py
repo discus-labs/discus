ad4b70638573f58606b832913ecea37371d32edd
Rohan Shah <128310345+rohankshah04@users.noreply.github.com>
2023-07-31 05:38:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='Discus',
     version='0.1.1b0',
-    description='A package containing everything you need to finetune your LLMs',
-    url='https://github.com/discus-labs/discus-synthetics.git',
+    description='generate high-quality data to fine-tune LLMs',
+    url='https://github.com/discus-labs/discus.git',
     author='Discus Founders',
     author_email='founders@discus.ai',
     packages=['discus'],
