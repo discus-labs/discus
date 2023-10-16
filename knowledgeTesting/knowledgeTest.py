a08d1107f694a7a462f0e1182f2c61354ff3f3b1
Nirav Shah <1413alok11@gmail.com>
2023-10-16 14:43:33 -0400

    redone tests and exception handling

---
+++ knowledgeTesting/knowledgeTest.py
@@ -0,0 +1,52 @@
+from discus import Knowledge
+import os
+import pytesseract
+
+from PIL import Image
+from pdf2image import convert_from_path
+
+
+# k = Knowledge(config = "/Users/NiravShah/Downloads/discus/examples/eng2spanish/eng2spanish.json")
+# raw_data = k.extract_data("/Users/NiravShah/Downloads/discus/knowledgeTesting/data")
+# print(len(raw_data))
+# print(raw_data)
+
+def extract_data(path):
+        """TODO: add docstring"""
+        """TODO: extract metadata"""
+        #supports txts and pdfs 
+        
+        raw_data = {}
+
+        for file_name in os.listdir(path):
+            file_path = os.path.join(path, file_name)
+            file_extension = os.path.splitext(file_path)[1].lower()
+            content = ""
+            metadata = {}
+
+            try:
+                if file_extension == ".txt":
+                    with open(file_path, "r") as file:
+                        content = file.read()
+                elif file_extension == ".pdf":
+                    images = convert_from_path(file_path)
+                    for image in images:
+                        text = pytesseract.image_to_string(image)
+                        content += text
+                else:
+                    raise IOError
+                
+                if content:
+                    raw_data[file_path] = (metadata, content)
+
+            except IOError as e:
+                print(f"{file_path} is unreadable (hint: make sure your knowledge base directory only contains files of type .txt or .pdf")
+
+
+        return raw_data
+
+
+raw_data = extract_data("/Users/NiravShah/Downloads/discus/knowledgeTesting/data")
+print(len(raw_data))
+print(raw_data)
+
