552b1a1eff2d36a2d062698dadd307a05422ad1d
Nirav Shah <1413alok11@gmail.com>
2023-11-18 12:15:23 -0500

    Remove unwanted directory

--- examples/eng2spanish/eng2spanish.py
+++
@@ -1,9 +0,0 @@
-import os
-from discus import Generator, Dataset
-
-discus = Generator(config = "/Users/rohankshah/Desktop/discus/examples/eng2spanish/eng2spanish.json")
-generated_data = discus.run()
-print(generated_data)
-
-# gen = Dataset(generated_data)
-# result = gen.n_most_unique_elements(2)
